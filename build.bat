cbb583624a574f63361f714603f2bc1268ccccf0
kaighe <kaialbertson@gmail.com>
2025-03-31 08:05:20 -0700

    switched project building to cmake

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-mkdir "build/include"
-mkdir "build/lib"
-mkdir "temp"
-
-g++ -Iinclude -Llib -c -o temp/trcc.o src/trcc.cpp -l"usb-1.0"
-g++ -Iinclude -Llib -o build/lib/trcc.dll temp/trcc.o -s -shared -Wl,--subsystem,windows -l"usb-1.0"
-@REM ar rcs build/libtrcc.a temp/trcc.o
